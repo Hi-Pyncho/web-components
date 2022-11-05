cb4e57d959633eb53f9406aaafa137eb7e9a4c90
Hi-Pyncho <garrakombo@gmail.com>
2022-11-05 17:54:16 +0300

    DELETE: commentaries from template

--- web-components/basic-template-without-commentary.js
+++ web-components/basic-template-without-commentary.js
@@ -18,23 +18,19 @@ customElements.define('p-modal', class extends LitElement {
     this.property = false
   }
 
-  // Invoked when a component is added to the document's DOM
   connectedCallback() {
     super.connectedCallback()
     // code...
   }
 
-  // Invoked when a component is removed from the document's DOM
   disconnectedcallback() {
     super.disconnectedcallback()
     // code...
   }
 
-  // Called after the component's DOM has been updated the first time
   firstupdated() {
     // code...
   }
-  
 
   getSlottedByName(name = '') {
     const search = name.trim() === '' ? 'slot' : `slot[name='${name}']`
